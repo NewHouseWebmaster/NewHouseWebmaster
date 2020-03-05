16febca3e65f589263f927c68e99477f42b5e075
Claire Cheng <ccheng00@mit.edu>
2020-03-04 19:38:57 -0500

    new build from source yay

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb9177ea4c8865a758619c470bf4bc52.js"
+  "/precache-manifest.5f4d652c5061572fe48b63b28bcc8c27.js"
 );
 
 self.addEventListener('message', (event) => {
